3059b2ca9ac7b2f35785a72e90c0fa0b83aed772
Archi Jain <jainarchi023@gmail.com>
2026-01-26 15:10:11 +0530

    Add Contact page route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Services from "./pages/Services";
+import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
 // Personal Wellness Service Pages
@@ -37,6 +38,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
           
           {/* Personal Wellness Services */}
           <Route path="/services" element={<Services />} />
